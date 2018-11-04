7cb724f63a76ffc6ccb66dd98983e635b4ddc414
kaklakariada <github@chp1.net>
2018-11-04 12:41:20 +0100

    Add text ui

--- textui/src/main/java/org/itsallcode/whiterabbit/textui/App.java
+++ textui/src/main/java/org/itsallcode/whiterabbit/textui/App.java
@@ -1,6 +1,7 @@
 package org.itsallcode.whiterabbit.textui;
 
 import java.nio.file.Paths;
+import java.util.Optional;
 
 import org.itsallcode.whiterabbit.logic.Config;
 import org.itsallcode.whiterabbit.logic.service.AppService;
@@ -10,12 +11,59 @@ import org.itsallcode.whiterabbit.logic.storage.Storage;
 
 public class App {
 
+	private static final char COMMAND_REPORT = 'r';
+	private static final char COMMAND_UPDATE = 'u';
+	static final char COMMAND_QUIT = 'q';
+
+	private final AppService appService;
+	private final UiTerminal terminal;
+
+	public App(AppService appService, UiTerminal terminal) {
+		this.appService = appService;
+		this.terminal = terminal;
+	}
+
 	public static void main(String[] args) {
-		final Config config = Config.read(Paths.get("../time.properties"));
+		final Config config = Config.read(Paths.get("time.properties"));
 		final Storage storage = new Storage(new DateToFileMapper(config.getDataDir()));
 		final AppService appService = new AppService(storage, new ClockService());
+		final UiTerminal terminal = UiTerminal.create();
+		new App(appService, terminal).run();
+	}
 
-		appService.update();
-		appService.report();
+	private void run() {
+		while (true) {
+			final Optional<Character> command = promptUser();
+			if (!command.isPresent()) {
+				continue;
+			}
+			final char commandChar = command.get().charValue();
+			if (commandChar == COMMAND_QUIT) {
+				return;
+			}
+			if (Character.isWhitespace(commandChar)) {
+				continue;
+			}
+			executeCommand(commandChar);
+		}
+	}
+
+	private void executeCommand(final char command) {
+		switch (command) {
+		case COMMAND_UPDATE:
+			appService.update();
+			break;
+		case COMMAND_REPORT:
+			appService.report();
+			break;
+		default:
+			System.err.println("Unknown command '" + command + "'");
+			break;
+		}
+	}
+
+	private Optional<Character> promptUser() {
+		System.out.println("Press key. u = update, r = report, q = quit");
+		return terminal.getNextCommand();
 	}
 }
---
+++ textui/src/main/java/org/itsallcode/whiterabbit/textui/AppException.java
@@ -0,0 +1,14 @@
+package org.itsallcode.whiterabbit.textui;
+
+public class AppException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public AppException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public AppException(String message) {
+		super(message);
+	}
+}
