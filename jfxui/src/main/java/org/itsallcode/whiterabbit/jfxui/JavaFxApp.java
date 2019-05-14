c428864dc24892ddab6a1f69bebfae9b8e3d7274
kaklakariada <kaklakariada@users.noreply.github.com>
2019-05-14 15:23:11 +0200

    Add table to jfxui

--- jfxui/src/main/java/org/itsallcode/whiterabbit/jfxui/JavaFxApp.java
+++ jfxui/src/main/java/org/itsallcode/whiterabbit/jfxui/JavaFxApp.java
@@ -2,18 +2,30 @@ package org.itsallcode.whiterabbit.jfxui;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Month;
+import java.time.YearMonth;
 import java.util.Locale;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.itsallcode.whiterabbit.logic.Config;
+import org.itsallcode.whiterabbit.logic.model.DayRecord;
 import org.itsallcode.whiterabbit.logic.service.AppService;
 import org.itsallcode.whiterabbit.logic.service.FormatterService;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
 public class JavaFxApp extends Application
@@ -21,6 +33,8 @@ public class JavaFxApp extends Application
     private static final Logger LOG = LogManager.getLogger(App.class);
 
     private AppService appService;
+
+    private final ObservableList<DayRecord> dayRecords = FXCollections.observableArrayList();
 
     @Override
     public void init() throws Exception
@@ -36,11 +50,65 @@ public class JavaFxApp extends Application
     public void start(Stage primaryStage) throws Exception
     {
         LOG.info("Starting UI");
-        final StackPane root = new StackPane(new Label("Hello World!"));
+        final GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        final ColumnConstraints colConstraint = new ColumnConstraints();
+        colConstraint.setPercentWidth(100);
+        grid.getColumnConstraints().add(colConstraint);
+        final RowConstraints rowConstraint = new RowConstraints();
+        rowConstraint.setPercentHeight(100);
+        grid.getRowConstraints().add(rowConstraint);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        grid.setGridLinesVisible(true);
 
-        final Scene scene = new Scene(root, 800, 600);
+        final Button updateButton = new Button("Update");
+        updateButton.setOnAction(e -> appService.updateNow());
+        appService.setUpdateListener(this::recordUpdated);
+        appService.startAutoUpdate();
 
+        fillRecords();
+        final TableView<DayRecord> table = new TableView<>(dayRecords);
+        table.setEditable(true);
+
+        table.getColumns().add(tableColumn("Date", "date"));
+        table.getColumns().add(tableColumn("Type", "type"));
+        table.getColumns().add(tableColumn("Begin", "begin"));
+        table.getColumns().add(tableColumn("End", "end"));
+        table.getColumns().add(tableColumn("Break", "mandatoryBreak"));
+        table.getColumns().add(tableColumn("Interruption", "interruption"));
+        table.getColumns().add(tableColumn("Comment", "comment"));
+
+        grid.add(updateButton, 0, 1);
+        GridPane.setFillHeight(table, true);
+        GridPane.setFillWidth(table, true);
+        grid.add(table, 0, 0);
+
+        final Scene scene = new Scene(grid, 800, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    private void fillRecords()
+    {
+        appService.getRecords(YearMonth.of(2019, Month.MAY)).stream().forEach(this::recordUpdated);
+    }
+
+    private <T> TableColumn<DayRecord, T> tableColumn(String label, String property)
+    {
+        final TableColumn<DayRecord, T> dateColumn = new TableColumn<>(label);
+        dateColumn.setCellValueFactory(new PropertyValueFactory<>(property));
+        return dateColumn;
+    }
+
+    private void recordUpdated(DayRecord record)
+    {
+        final int recordIndex = record.getDate().getDayOfMonth() - 1;
+        while (dayRecords.size() <= recordIndex)
+        {
+            dayRecords.add(null);
+        }
+        dayRecords.set(recordIndex, record);
     }
 }
