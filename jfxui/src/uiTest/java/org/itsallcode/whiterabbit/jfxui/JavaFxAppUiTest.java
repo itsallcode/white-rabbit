58af0a7c128b3b98e12133010d17fd46e0bf0966
kaklakariada <kaklakariada@users.noreply.github.com>
2020-08-15 08:05:24 +0200

    Get trigger runnables

--- jfxui/src/uiTest/java/org/itsallcode/whiterabbit/jfxui/JavaFxAppUiTest.java
+++ jfxui/src/uiTest/java/org/itsallcode/whiterabbit/jfxui/JavaFxAppUiTest.java
@@ -3,6 +3,8 @@ package org.itsallcode.whiterabbit.jfxui;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
@@ -10,22 +12,22 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Clock;
+import java.time.Instant;
 import java.time.ZoneId;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 import org.itsallcode.whiterabbit.jfxui.table.days.DayRecordPropertyAdapter;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.io.TempDir;
+import org.mockito.ArgumentCaptor;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.testfx.api.FxRobot;
 import org.testfx.assertions.api.Assertions;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
-import org.testfx.framework.junit5.Stop;
 
 import javafx.scene.control.TableView;
 import javafx.stage.Stage;
@@ -42,44 +44,41 @@ class JavaFxAppUiTest
     Path dataDir;
     Clock clockMock;
     ScheduledExecutorService executorServiceMock;
+    Instant now = Instant.parse("2007-12-03T10:15:30.20Z");
 
-    private ScheduledExecutorService realExecutor;
+    private Runnable updateEverySecondRunnable;
+
+    private Runnable updateEveryMinuteRunnable;
 
     @Start
     void start(Stage stage) throws IOException
     {
         clockMock = mock(Clock.class);
         executorServiceMock = mock(ScheduledExecutorService.class);
-        final ScheduledFuture scheduledFutureMock = mock(ScheduledFuture.class);
+        final ScheduledFuture<?> scheduledFutureMock = mock(ScheduledFuture.class);
+
+        when(executorServiceMock.schedule(any(Runnable.class), eq(0L), eq(TimeUnit.MILLISECONDS)))
+                .thenAnswer(invocation -> scheduledFutureMock);
 
         javaFxApp = new JavaFxApp(() -> workingDir, clockMock, executorServiceMock);
 
         when(clockMock.getZone()).thenReturn(ZoneId.of("Europe/Berlin"));
-        // when(clockMock.instant()).thenReturn(Instant.parse("2007-12-03T10:15:30.00Z"));
-        when(clockMock.instant()).thenAnswer(invocation -> Clock.systemDefaultZone().instant());
+        when(clockMock.instant()).thenAnswer(invocation -> now);
 
-        realExecutor = new ScheduledThreadPoolExecutor(1);
-        when(executorServiceMock.schedule(any(Runnable.class), any(Long.class), eq(TimeUnit.MILLISECONDS)))
-                .thenAnswer(invocation -> {
-                    final Runnable runnable = invocation.getArgument(0,
-                            Runnable.class);
-                    final Long delay = invocation.getArgument(1, Long.class);
-                    final TimeUnit timeUnit = invocation.getArgument(2,
-                            TimeUnit.class);
-                    System.out.println("Schedule " + runnable + " with delay " + delay + " " + timeUnit);
-                    return realExecutor.schedule(runnable, delay, timeUnit);
-                });
         prepareConfiguration();
 
         javaFxApp.init();
         javaFxApp.start(stage);
+
+        captureScheduledRunnables();
     }
 
-    @Stop
-    void stop()
+    private void captureScheduledRunnables()
     {
-        javaFxApp.stop();
-        realExecutor.shutdown();
+        final ArgumentCaptor<Runnable> arg = ArgumentCaptor.forClass(Runnable.class);
+        verify(executorServiceMock, times(2)).schedule(arg.capture(), eq(0L), eq(TimeUnit.MILLISECONDS));
+        updateEverySecondRunnable = arg.getAllValues().get(0);
+        updateEveryMinuteRunnable = arg.getAllValues().get(1);
     }
 
     private void prepareConfiguration() throws IOException
@@ -96,6 +95,6 @@ class JavaFxAppUiTest
     {
         final TableView<DayRecordPropertyAdapter> dayTable = robot.lookup("#day-table").queryTableView();
         Assertions.assertThat(dayTable).hasExactlyNumRows(31);
-        Thread.sleep(5000);
+        Thread.sleep(3000);
     }
 }
