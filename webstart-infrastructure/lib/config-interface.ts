ac6bf3d2c748a46e15477d338b908c1255e5cf00
kaklakariada <kaklakariada@users.noreply.github.com>
2020-10-25 16:31:08 +0100

    Add static website

---
+++ webstart-infrastructure/lib/config-interface.ts
@@ -0,0 +1,7 @@
+export interface InfrastructureConfig {
+    region: string;
+    domain: string;
+    hostedZoneName: string;
+    sslCertificateArn: string;
+    stackName: string;
+}
--- webstart-infrastructure/lib/infrastructure-stack.ts
+++ webstart-infrastructure/lib/infrastructure-stack.ts
@@ -1,9 +1,20 @@
 import * as cdk from '@aws-cdk/core';
+import { StaticContentConstruct } from './static-content';
+
+export interface InfrastructureStackProps extends cdk.StackProps {
+  domain: string;
+  hostedZoneName: string;
+  sslCertificateArn: string;
+}
 
 export class InfrastructureStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props: InfrastructureStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new StaticContentConstruct(this, 'StaticContent', {
+      domain: props.domain,
+      hostedZoneName: props.hostedZoneName,
+      sslCertificateArn: props.sslCertificateArn
+    });
   }
 }
