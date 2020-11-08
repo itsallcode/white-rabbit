1706be6e9c1131ac0d659f80ebb92b9ac024eb11
kaklakariada <kaklakariada@users.noreply.github.com>
2020-11-08 11:31:02 +0100

    Move cdk starter to avoid problems with bin folder

--- webstart-infrastructure/webstart-infrastructure.ts
+++ webstart-infrastructure/webstart-infrastructure.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import cdk = require("@aws-cdk/core");
-import { InfrastructureStack, InfrastructureStackProps } from "../lib/infrastructure-stack";
-import { CONFIG } from "../config";
-import { InfrastructureConfig } from "../lib/config-interface";
+import { InfrastructureStack, InfrastructureStackProps } from "./lib/infrastructure-stack";
+import { CONFIG } from "./config";
+import { InfrastructureConfig } from "./lib/config-interface";
 
 const config: InfrastructureConfig = CONFIG;
 
