db60675fc00b6ba0bd0289b7cbbe46cce06501af
kaklakariada <kaklakariada@users.noreply.github.com>
2021-01-29 14:43:55 +0100

    Redirect jul to log4j2

--- jfxui/src/main/java/org/itsallcode/whiterabbit/jfxui/App.java
+++ jfxui/src/main/java/org/itsallcode/whiterabbit/jfxui/App.java
@@ -10,6 +10,7 @@ public class App
     {
         System.setProperty("javafx.preloader", SplashScreenLoader.class.getName());
         System.setProperty("log4j.skipJansi", "false");
+        System.setProperty("java.util.logging.manager", "org.apache.logging.log4j.jul.LogManager");
         Application.launch(JavaFxApp.class, args);
     }
 }
