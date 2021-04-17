05aa8ee9358f13cf31ffa157886409e85313f25b
kaklakariada <kaklakariada@users.noreply.github.com>
2021-04-17 16:48:58 +0200

    Add module-info

---
+++ api/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module org.itsallcode.whiterabbit.api
+{
+    exports org.itsallcode.whiterabbit.api.model;
+    exports org.itsallcode.whiterabbit.api.features;
+    exports org.itsallcode.whiterabbit.api;
+}
