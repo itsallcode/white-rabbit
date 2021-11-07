400244480c0c0a7bb701169a687cc3e026aabb54
kaklakariada <kaklakariada@users.noreply.github.com>
2021-11-07 11:58:56 +0100

    Fix opening files on Ubuntu
    See https://bugs.openjdk.java.net/browse/JDK-8096706?focusedCommentId=13785982&page=com.atlassian.jira.plugin.system.issuetabpanels%3Acomment-tabpanel#comment-13785982

--- jfxui/src/main/java/org/itsallcode/whiterabbit/jfxui/service/DesktopService.java
+++ jfxui/src/main/java/org/itsallcode/whiterabbit/jfxui/service/DesktopService.java
@@ -3,14 +3,20 @@ package org.itsallcode.whiterabbit.jfxui.service;
 import java.awt.Desktop;
 import java.nio.file.Path;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public interface DesktopService
 {
     static DesktopService create()
     {
+        final Logger log = LogManager.getLogger(DesktopService.class);
         if (Desktop.isDesktopSupported())
         {
+            log.info("Desktop is supported, use real desktop service");
             return new RealDesktopService(Desktop.getDesktop());
         }
+        log.info("Desktop is not supported, use fake desktop service");
         return new FakeDesktopService();
     }
 
