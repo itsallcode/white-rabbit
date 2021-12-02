79131fa2a8bae5e504aa42093310272967c0f20e
Christoph Kuhnke <github@kuhnke.net>
2021-12-02 12:01:19 +0100

    #206  fixed sonar findings in test

--- plugins/pmsmart/src/test/java/org/itsallcode/whiterabbit/plugin/pmsmart/web/WebDriverKeyTest.java
+++ plugins/pmsmart/src/test/java/org/itsallcode/whiterabbit/plugin/pmsmart/web/WebDriverKeyTest.java
@@ -4,12 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
-public class WebDriverKeyTest
+class WebDriverKeyTest
 {
     @Test
     void repeat()
     {
         final WebDriverKey key = WebDriverKey.BACKSPACE;
-        assertThat(key.repeat(20).toString().length()).isEqualTo(key.toString().length() * 20);
+        assertThat(key.repeat(20).toString()).hasSize(key.toString().length() * 20);
     }
 }
