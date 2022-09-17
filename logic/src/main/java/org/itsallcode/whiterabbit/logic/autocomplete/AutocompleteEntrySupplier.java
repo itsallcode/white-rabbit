c4cae2fff19be34b66f803bbc00c02223cb74d45
kaklakariada <kaklakariada@users.noreply.github.com>
2022-09-17 18:45:00 +0200

    #250: Update autocomplete proposals on updates

--- logic/src/main/java/org/itsallcode/whiterabbit/logic/autocomplete/AutocompleteEntrySupplier.java
+++ logic/src/main/java/org/itsallcode/whiterabbit/logic/autocomplete/AutocompleteEntrySupplier.java
@@ -2,6 +2,7 @@ package org.itsallcode.whiterabbit.logic.autocomplete;
 
 import java.util.List;
 
+@FunctionalInterface
 public interface AutocompleteEntrySupplier
 {
     List<AutocompleteProposal> getEntries(String currentText);
