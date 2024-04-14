d9a599edf5282302fce575852f7b1d3d86597985
kaklakariada <kaklakariada@users.noreply.github.com>
2024-04-14 14:54:52 +0200

    Poll for condition

--- jfxui/src/uiTest/java/org/itsallcode/whiterabbit/jfxui/testutil/TestUtil.java
+++ jfxui/src/uiTest/java/org/itsallcode/whiterabbit/jfxui/testutil/TestUtil.java
@@ -2,8 +2,13 @@ package org.itsallcode.whiterabbit.jfxui.testutil;
 
 import java.time.Duration;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class TestUtil
 {
+    private static final Logger LOG = LogManager.getLogger(TestUtil.class);
+
     private TestUtil()
     {
         // Not instantiable
@@ -19,7 +24,7 @@ public class TestUtil
         sleep(Duration.ofMillis(100));
     }
 
-    private static void sleep(Duration duration)
+    private static void sleep(final Duration duration)
     {
         try
         {
@@ -28,6 +33,28 @@ public class TestUtil
         catch (final InterruptedException e)
         {
             Thread.currentThread().interrupt();
+        }
+    }
+
+    public static void retryAssertion(final Duration duration, final Runnable assertion)
+    {
+        final long start = System.currentTimeMillis();
+        while (true)
+        {
+            try
+            {
+                assertion.run();
+                return;
+            }
+            catch (final AssertionError e)
+            {
+                LOG.warn("Assertion failed: {}", e.getMessage(), e);
+                if (System.currentTimeMillis() - start > duration.toMillis())
+                {
+                    throw e;
+                }
+                sleepShort();
+            }
         }
     }
 }
--- jfxui/src/uiTest/java/org/itsallcode/whiterabbit/jfxui/testutil/model/MonthlyProjectReportWindow.java
+++ jfxui/src/uiTest/java/org/itsallcode/whiterabbit/jfxui/testutil/model/MonthlyProjectReportWindow.java
@@ -5,11 +5,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.time.Duration;
 
 import org.itsallcode.whiterabbit.api.model.Project;
+import org.itsallcode.whiterabbit.jfxui.testutil.TestUtil;
 import org.itsallcode.whiterabbit.jfxui.ui.MonthlyProjectReportViewer;
 import org.itsallcode.whiterabbit.jfxui.ui.MonthlyProjectReportViewer.ReportRow;
 import org.testfx.api.FxRobot;
 import org.testfx.assertions.api.Assertions;
-import org.testfx.util.WaitForAsyncUtils;
 
 import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
@@ -39,13 +39,11 @@ public class MonthlyProjectReportWindow
     public void gotoPreviousMonth()
     {
         robot.clickOn("#prev-month-button");
-        WaitForAsyncUtils.waitForFxEvents();
     }
 
     public void gotoNextMonth()
     {
         robot.clickOn("#next-month-button");
-        WaitForAsyncUtils.waitForFxEvents();
     }
 
     public void closeViaEscKey()
@@ -57,7 +55,7 @@ public class MonthlyProjectReportWindow
     public MonthlyProjectReportWindow assertWindowTitle(final String expectedTitle)
     {
         final String title = ((Stage) window).getTitle();
-        assertThat(title).isEqualTo(expectedTitle);
+        TestUtil.retryAssertion(Duration.ofSeconds(5), () -> assertThat(title).isEqualTo(expectedTitle));
         return this;
     }
 
